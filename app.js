71355e6c8d7666409aee372def36697015a378a3
zyz <zyzisgood@gmail.com>
2020-04-18 21:51:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const serverHandle = (req,res)=> {
+    res.setHeader('Content-type','application/json');
+
+    const resData = {
+        name:"yi",
+        site:'zyz',
+        env:process.env.NODE_ENV
+    }
+
+    res.end(
+        JSON.stringify(resData)
+    )
+}
+
+module.exports = serverHandle;
